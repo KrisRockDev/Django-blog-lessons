22e6dfc2f239f815557335590b2891747e66039e
KrisRock <_>
2023-01-06 19:55:36 +0300

    Описал модель новостей в файле models.py. Перенес новости на главную страницу, изменён urls.py.

--- mysite/news/models.py
+++ mysite/news/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class News(models.Model):
+    title = models.CharField(max_length=150)
+    content = models.TextField(blank=True) # blank=True поле не обязательно для заполнения
+    created_at = models.DateTimeField(auto_now_add=True) # поле заполняет дату и время в момент создания
+    updated_at = models.DateTimeField(auto_now=True) # поле заполняется при изменении новости
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/')
+    is_published = models.BooleanField(default=True)
