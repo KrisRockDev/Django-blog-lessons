47dbcf547e6982d4256e071dcff99ae876879674
KrisRock <_>
2023-01-07 11:17:26 +0300

    Исправлена ошибка в файле 'urls.py'

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -24,5 +24,5 @@ urlpatterns = [
 ]
 
 # Формирование маршрута сохранения медиафайлов в отладочном режиме и на реальном сервере
-if settings.DEBAG:
+if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
