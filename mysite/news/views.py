8eeff273f37f9f589a0afaacfb5817e992d79a63
KrisRock <_>
2023-01-07 11:51:43 +0300

    Создана простой шаблон в 'view.py' на базе 'index.html' для отображения всех данных из базы, выполнена руссификация.

--- mysite/news/views.py
+++ mysite/news/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import News
+
+
 def index(request):
-    news = News.objects.all()
-    res = '<h1>Список новостей</h1>\n<hr>\n'
-    for item in news:
-        res += f'<div>\n<p><h3>{item.title}</h3></p>\n<p>{item.content}</p>\n<div>\n<hr>\n'
-    return HttpResponse(res)
+    news = News.objects.order_by('-created_at')
+    return render(
+        request=request,
+        template_name='news/index.html',
+        context={
+            'news': news,
+            'title': 'Список новостей'
+        },
+    )
