634d505aef11b3416a624b926e9be8d4593aae32
KrisRock <_>
2023-01-07 13:38:04 +0300

    Выполнены настройки приложения в админке. Медиафайлы включены в файл .gitignore

--- mysite/news/admin.py
+++ mysite/news/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import News
 
-# Register your models here.
+
+class NewsAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'content', 'created_at', 'updated_at', 'is_published',)
+    list_display_links = ('id', 'title', ) # Добавляет ссылки на объект
+    search_fields = ('title', 'content', ) # Добавляет поиск по содержимому полей
+
+# Порядок регистрации важен, сначала регистрируется модель, потом её представление
+admin.site.register(News, NewsAdmin)
--- mysite/news/models.py
+++ mysite/news/models.py
@@ -1,13 +1,18 @@
 from django.db import models
 
 class News(models.Model):
-    title = models.CharField(max_length=150)
-    content = models.TextField(blank=True) # blank=True поле не обязательно для заполнения
-    created_at = models.DateTimeField(auto_now_add=True) # поле заполняет дату и время в момент создания
-    updated_at = models.DateTimeField(auto_now=True) # поле заполняется при изменении новости
-    photo = models.ImageField(upload_to='photos/%Y/%m/%d/')
-    is_published = models.BooleanField(default=True)
+    title = models.CharField(max_length=150, verbose_name = 'Наименование')
+    content = models.TextField(blank=True, verbose_name = 'Контент') # blank=True поле не обязательно для заполнения
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name = 'Дата публикации') # поле заполняет дату и время в момент создания
+    updated_at = models.DateTimeField(auto_now=True, verbose_name = 'Обновлено') # поле заполняется при изменении новости
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/', verbose_name = 'Фото', blank=True,)
+    is_published = models.BooleanField(default=True, verbose_name = 'Опубликовано')
 
     # Возвращаем строковое представление объектов вместо <QuerySet>
     def __str__(self):
         return self.title
+
+    class Meta:
+        verbose_name = 'Новость'
+        verbose_name_plural = 'Новости'
+        ordering = ['-created_at', ]
