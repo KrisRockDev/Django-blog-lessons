43977c85f184bd7a4a42965c660094fc0fd2f1b3
KrisRock <_>
2023-01-07 11:25:25 +0300

    Создана простая вьюха (без шаблона) в 'view.py' для отображения всех данных из базы

--- mysite/news/views.py
+++ mysite/news/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import News
 def index(request):
-    return HttpResponse('Hello world')
+    news = News.objects.all()
+    res = '<h1>Список новостей</h1>\n<hr>\n'
+    for item in news:
+        res += f'<div>\n<p><h3>{item.title}</h3></p>\n<p>{item.content}</p>\n<div>\n<hr>\n'
+    return HttpResponse(res)
