336321f42522e1d12f02d1f5bf8992e101954d25
KrisRock <_>
2023-01-09 16:55:50 +0300

    Созданы URL с параметрами. Добавлено меню категорий и фильтрация по ним.

--- mysite/news/views.py
+++ mysite/news/views.py
@@ -1,15 +1,41 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import News
+from .models import News, Category
 
 
 def index(request):
     news = News.objects.all()
+    categories = Category.objects.all()
+    cat_count = {}
+
+    CONTEXT = {
+        'news': news,
+        'categories': categories,
+        'cat_count': cat_count,
+        'title': 'Новостной портал',
+    }
+
     return render(
         request=request,
         template_name='news/index.html',
-        context={
-            'news': news,
-            'title': 'Новостной портал',
-        },
+        context=CONTEXT,
+    )
+
+
+def get_category(request, category_id):
+    news = News.objects.filter(category_id=category_id)
+    categories = Category.objects.all()
+    category = Category.objects.get(pk=category_id)
+
+    CONTEXT = {
+        'news': news,
+        'categories': categories,
+        'category': category,
+        'title': 'Новостной портал',
+    }
+
+    return render(
+        request=request,
+        template_name='news/category.html',
+        context=CONTEXT,
     )
