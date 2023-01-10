665fd2483e12ac22937c216c389e47c5dc61dc10
KrisRock <_>
2023-01-10 11:12:03 +0300

    Созданы пользовательские теги шаблона sitebar двумя способами

--- mysite/news/views.py
+++ mysite/news/views.py
@@ -5,12 +5,10 @@ from .models import News, Category
 
 def index(request):
     news = News.objects.all()
-    categories = Category.objects.all()
     cat_count = {}
 
     CONTEXT = {
         'news': news,
-        'categories': categories,
         'cat_count': cat_count,
         'title': 'Новостной портал',
     }
@@ -24,12 +22,10 @@ def index(request):
 
 def get_category(request, category_id):
     news = News.objects.filter(category_id=category_id)
-    categories = Category.objects.all()
     category = Category.objects.get(pk=category_id)
 
     CONTEXT = {
         'news': news,
-        'categories': categories,
         'category': category,
         'title': 'Новостной портал',
     }
